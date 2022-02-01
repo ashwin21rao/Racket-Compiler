cfdd627008c43a5d4800f08ef215704a42d553f8
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2022-02-01 23:22:26 +0530

    Working on RCO

--- compiler.rkt
+++ compiler.rkt
@@ -68,7 +68,7 @@
       [(Var x) (Var (dict-ref env x))]
       [(Int n) (Int n)]
       [(Let x e body) (let* ([new_x (gensym x)] [new_env (dict-set env x new_x)])
-                         (Let new_x ((uniquify-exp env) e) ((uniquify-exp new_env) body)))]
+                        (Let new_x ((uniquify-exp env) e) ((uniquify-exp new_env) body)))]
       [(Prim op es) (Prim op
                           (for/list ([e es])
                             ((uniquify-exp env) e)))])))
@@ -79,8 +79,54 @@
     [(Program info e) (Program info ((uniquify-exp '()) e))]))
 
 ;; remove-complex-opera* : R1 -> R1
+
+(define (rco-atom expr)
+  (match expr
+    [(Var x) (list (list (Var x) '()))]
+    [(Int x) (list (list (Int x) '()))]
+    [(Let x e body) (Let x (rco-exp e)
+                      (rco-exp body))]
+    [(Prim op (list exp1 exp2)) (let* ([vs1 (rco-atom exp1)]
+                                [vs2 (rco-atom exp2)]
+                                [atom1 (car (last vs1))]
+                                [atom2 (car (last vs2))]
+                                [new_var (Var (gensym 'temp))]
+                                [new_prim (Prim op (list atom1 atom2))]
+                                [new_ele (list new_var new_prim)])
+                           (append vs1 vs2 (list new_ele)))]
+    [(Prim op (list exp1)) (let* ([vs1 (rco-atom exp1)]
+                           [atom1 (car (last vs1))]
+                           [new_var (gensym 'temp)]
+                           [new_prim (Prim op (list atom1))]
+                           [new_ele (list new_var new_prim)])
+                      (append vs1 (list new_ele)))]))
+
+;; (+ (+ 42 10) (- 10))
+;; ((tmp1, (+ 42 10), (tmp2, (- 10)))) + tmp1 tmp2
+#| (define (gen-lets lst) |#
+#|   (match lst |#
+#|     [(list (list a b)) b] |#
+#|     [(list (list a b) ...) (Let a b (gen-lets (rest lst)))] |#
+#|   )) |#
+;; ((temp, (+ 20 10)))
+(require racket/trace)
+(trace-define (gen-lets lst)
+  (cond 
+    [(= 1 (length lst)) (cadar lst)]
+    [else (Let (caar lst) (cdar lst) (gen-lets (rest lst)))]
+    ))
+
+(define (rco-exp e)
+  (match e
+    [(Var x) (Var x)]
+    [(Int x) (Int x)]
+    [(Prim 'read '()) (Prim 'read '())]
+    [(Let x e body) (Let x (rco-exp e) (rco-exp body))]
+    [(Prim op es) (gen-lets (rco-atom (Prim op es)))]))
+
 (define (remove-complex-opera* p)
-  (error "TODO: code goes here (remove-complex-opera*)"))
+  (match p
+    [(Program info e) (Program info (rco-exp e))]))
 
 ;; explicate-control : R1 -> C0
 (define (explicate-control p)
@@ -108,7 +154,7 @@
 (define compiler-passes
   `(("uniquify" ,uniquify ,interp-Lvar)
     ;; Uncomment the following passes as you finish them.
-    ;; ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar)
+    ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar)
     ;; ("explicate control" ,explicate-control ,interp-Cvar)
     ;; ("instruction selection" ,select-instructions ,interp-x86-0)
     ;; ("assign homes" ,assign-homes ,interp-x86-0)
