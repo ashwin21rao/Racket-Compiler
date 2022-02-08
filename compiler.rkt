b40356ab325cb312992398dfc211689fa2bbc387
Pulak Malhotra <pulak.malhotra@students.iiit.ac.in>
2022-02-08 15:00:14 +0530

    Minor improvements

--- compiler.rkt
+++ compiler.rkt
@@ -57,16 +57,6 @@
 ;; HW1 Passes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-#| (define (uniquify-exp env) |#
-#|   (lambda (e) |#
-#|     (match e |#
-#|       [(Var x) (Var x)] |#
-#|       [(Int n) (Int n)] |#
-#|       [(Let x e body) (Let x ((uniquify-exp env) e) ((uniquify-exp env) body))] |#
-#|       [(Prim op es) (Prim op |#
-#|                           (for/list ([e es]) |#
-#|                             ((uniquify-exp env) e)))]))) |#
-
 (define (uniquify-exp env)
   (lambda (e)
     (match e
@@ -83,7 +73,8 @@
   (match p
     [(Program info e) (Program info ((uniquify-exp '()) e))]))
 
-;; remove-complex-opera* : R1 -> R1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; return atom, list of variables
 (define (rco-atom expr)
@@ -123,14 +114,6 @@
                              (cons new_var new_vs))]
     ))
 
-;; (+ (+ 42 10) (- 10))
-;; ((tmp1, (+ 42 10), (tmp2, (- 10)))) + tmp1 tmp2
-#| (define (gen-lets lst) |#
-#|   (match lst |#
-#|     [(list (list a b)) b] |#
-#|     [(list (list a b) ...) (Let a b (gen-lets (rest lst)))] |#
-#|   )) |#
-;; ((temp, (+ 20 10)))
 (define (gen-lets lst)
   (cond
     [(= 1 (length lst)) (cadar lst)]
@@ -144,36 +127,40 @@
     [(Let x e body) (Let x (rco-exp e) (rco-exp body))]
     [(Prim op es) (gen-lets (cdr (rco-atom (Prim op es))))]))
 
+;; remove-complex-opera* : R1 -> R1
 (define (remove-complex-opera* p)
   (match p
     [(Program info e) (Program info (rco-exp e))]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; explicate-control : R1 -> C0
-;; explicate_tail : Lvar -> C0 tail
-(define (explicate_tail e)
+;; explicate-tail : Lvar -> C0 tail
+(define (explicate-tail e)
   (match e
     [(Var x) (Return (Var x))]
     [(Int n) (Return (Int n))]
-    [(Let x rhs body) (let* ([tail (explicate_tail body)] [nt (explicate_assign rhs x tail)]) nt)]
+    [(Let x rhs body) (let* ([tail (explicate-tail body)] [nt (explicate-assign rhs x tail)]) nt)]
     [(Prim op es) (Return (Prim op es))]
-    [else (error "explicate_tail unhandled case" e)]))
+    [else (error "explicate-tail unhandled case" e)]))
 
 ;; explicate_assign : Lvar, Var x, C0 tail -> C0 tail
-(define (explicate_assign e x cont)
+(define (explicate-assign e x cont)
   (match e
     [(Var y) (Seq (Assign (Var x) (Var y)) cont)]
     [(Int n) (Seq (Assign (Var x) (Int n)) cont)]
     [(Let y rhs body)
-     (let* ([tail (explicate_assign body x cont)] [nt (explicate_assign rhs y tail)]) nt)]
+     (let* ([tail (explicate-assign body x cont)] [nt (explicate-assign rhs y tail)]) nt)]
     [(Prim op es) (Seq (Assign (Var x) (Prim op es)) cont)]
-    [else (error "explicate_assign unhandled case" e)]))
+    [else (error "explicate-assign unhandled case" e)]))
 
 (define (explicate-control p)
   (match p
-    [(Program info body) (CProgram info (list (cons 'start (explicate_tail body))))]))
+    [(Program info body) (CProgram info (list (cons 'start (explicate-tail body))))]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; select-instructions : C0 -> pseudo-x86
-;; TODO implement read too
 (define (convert-to-x86 p)
   (match p
     [(Var x) (Var x)]
@@ -195,40 +182,46 @@
     [(Return e) (let * ([instrs (convert-to-x86 (Assign (Reg 'rax) e))] [instr2 (Jmp 'conclusion)])
                   (append instrs (list instr2)))]))
 
+(define (select-instructions-block blck)
+  (match blck
+    [(cons label cmds) (cons label (Block '() (convert-to-x86 cmds)))]
+    )
+  )
 (define (select-instructions p)
   (match p
-    [(CProgram info blocks)
-     (X86Program info (list (cons 'start (Block '() (convert-to-x86 (dict-ref blocks 'start))))))]))
+    [(CProgram info blocks) (X86Program info (map select-instructions-block blocks))]))
 
-(define (set_variable var_list position)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (var-offset-mapping var_list position)
   (cond
     [(empty? var_list) (list)]
-    [else (cons (list (car var_list) position) (set_variable (cdr var_list) (- position 8)))]))
+    [else (cons (list (car var_list) position) (var-offset-mapping (cdr var_list) (- position 8)))]))
 
-(define (replace_inst_list inst_list var_list)
+(define (assign-home-instr-list inst_list var_stack_mapping)
   (for/list ([inst inst_list])
     (match inst
       [(Instr op args) (Instr op
                               (for/list ([arg args])
                                 (match arg
-                                  [(Var v) (Deref 'rbp (first (dict-ref var_list v)))]
+                                  [(Var v) (Deref 'rbp (first (dict-ref var_stack_mapping v)))]
                                   [else arg])))]
       [else inst])))
 
-(define (replace_vars cur_block var_list)
+(define (assign-homes-blocks cur_block var_stack_mapping)
   (match cur_block
-    [(Block info inst_list) (Block info (replace_inst_list inst_list var_list))]))
+    [(cons label (Block info instr_list)) (cons label (Block info (assign-home-instr-list instr_list var_stack_mapping)))]))
 
 ;; assign-homes : pseudo-x86 -> pseudo-x86
 (define (assign-homes p)
   (match p
     [(X86Program info blocks)
      (X86Program info
-                 (let* ([var_list (set_variable (dict-keys (dict-ref info 'locals-types)) -8)]
-                        [new_p (replace_vars (dict-ref blocks 'start) var_list)])
-                   (dict-set blocks 'start new_p)))]))
-; (error "TODO: code goes here (assign-homes)"))
+                 (let* ([var_stack_mapping (var-offset-mapping (dict-keys (dict-ref info 'locals-types)) -8)]
+                        [new_blocks (map (lambda (x) (assign-homes-blocks x var_stack_mapping)) blocks)])
+                   new_blocks))]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; patch-instructions : psuedo-x86 -> x86
 
 (define (fix-instructions inst_list)
@@ -251,6 +244,7 @@
 (define (patch-instructions p)
               (match p
                 [(X86Program info blocks) (X86Program info (map patch-block blocks))]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; prelude-and-conclusion : x86 -> x86
 (define (prelude-and-conclusion p)
@@ -271,6 +265,7 @@
                         [blocks (dict-set* blocks 'main main_block 'conclusion conclusion_block)])
                    blocks))]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Define the compiler passes to be used by interp-tests and the grader
 ;; Note that your compiler file (the file that defines the passes)
 ;; must be named "compiler.rkt"
